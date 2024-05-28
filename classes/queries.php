3b40b4510f8f6e80b7e767a0f59059979a3a8ad4
Di Depan Layar <albinmaulana@yahoo.com>
2024-05-28 17:12:31 +0700

    Feature: Row Count Method

--- classes/queries.php
+++ classes/queries.php
@@ -2,6 +2,14 @@
 
 class queries extends database {
     public $result;
+
+    /**
+     * CRUD Query Method
+     *
+     * @param [type] $query
+     * @param array $params
+     * @return void
+     */
     public function data($query, $params = []) {
         if(empty($params)) {
             $this->result = $this->connection->prepare($data);
@@ -10,6 +18,15 @@ class queries extends database {
             $this->result = $this->connection->prepare($data);
             return $this->result->execute($params);
         }
+    }
+    
+    /**
+     * Row Count Method
+     *
+     * @return void
+     */
+    public function rowCount() {
+        return $this->result->rowCount();
     }
 }
 
